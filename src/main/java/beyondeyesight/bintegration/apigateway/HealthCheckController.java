d29dfc4273c7aef3c17a2e23ad0aef1c32d119c2
junk-sound <wom2277@naver.com>
2022-06-04 14:51:44 +0900

    update

--- src/main/java/beyondeyesight/bintegration/apigateway/HealthCheckController.java
+++ src/main/java/beyondeyesight/bintegration/apigateway/HealthCheckController.java
@@ -1,13 +1,14 @@
 package beyondeyesight.bintegration.apigateway;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
-    @RequestMapping("/health-check/ping")
-    public String ping() {
-        return "health checked";
+    @RequestMapping("/health")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("health checked");
     }
 }
