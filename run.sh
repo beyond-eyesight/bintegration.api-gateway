341cd7e7bfb0b785e474699556df2023ec1062d6
junk-sound <wom2277@naver.com>
2022-06-04 14:31:43 +0900

    update

---
+++ run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo ">>>>>>>>>>>> Starting api gateway "
+echo "********************************************************"
+java -jar /usr/local/api-gateway/api-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=$1
